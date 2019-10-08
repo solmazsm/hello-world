6438287b69daf3fe30a94b31bc767cfab9296ab8
solmazsm <55103176+solmazsm@users.noreply.github.com>
2019-10-08 10:30:44 -0500

    web-solmaz

---
+++ web-solmaz/jspage1.js
@@ -0,0 +1,16 @@
+//script.js
+
+//$(function(){
+    
+// alert("How can insurance-company help you today?");
+   
+//});
+$(document).ready(function()
+                 {
+    $("buttonok").click(function(){
+        
+         window.location.href = "home.html";
+    return false;
+    });
+    
+});
